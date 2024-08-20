35a5c7332217ed3a911078e6058713a4be67c217
CastleLi <castle.lee.f@gmail.com>
2024-08-20 14:28:07 -0500

    separating homepage into two sections

--- Code/Mizzou 3D SPinE.R
+++
@@ -1,1 +0,0 @@
-if(!require("devtools")){install.packages("devtools")}; devtools::source_url("https://raw.githubusercontent.com/Mizzou-3d-Spine/Mizzou-3d-Spine/main/Code/Function.R");datamerge()
---
+++ Mizzou-3d-Spine-v1.0/Code/Mizzou 3D SPinE.R
@@ -0,0 +1,1 @@
+if(!require("devtools")){install.packages("devtools")}; devtools::source_url("https://raw.githubusercontent.com/Mizzou-3d-Spine/Mizzou-3d-Spine/main/Code/Function.R");datamerge()
